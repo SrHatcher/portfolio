6294d416d2435d0d7cda78ad7a2fe9b742bd2e4d
SrHatcher <erickomaldonado11@gmail.com>
2022-09-05 17:09:40 -0600

    creando la vista de about

--- portfolio/src/About/index.js
+++ portfolio/src/About/index.js
@@ -4,12 +4,18 @@ import "./about.css"
 function About(){
     return(
         <section className="about-container">
-            {/*
-            <div className="about_white-line" />
+            <React.Fragment>
+                <div className="about-lateral1" />
+                <div className="about-lateral2" />
+                <div className="about-lateral3" />
+                <div className="about-lateral4" />
+            </React.Fragment>
+            
             <div className="about-content">
-                
+                <div className="about-line"></div>
+                <h1><span>A</span>bout</h1>
+                <div className="about-bullets"></div>
             </div>
-    */}
         </section>
     )
 }
